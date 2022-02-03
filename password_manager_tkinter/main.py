ff7665d9c3c564f3c57f924327a94b565707ab26
Verevkin-A <sandyverevkin@gmail.com>
2022-02-03 17:49:43 +0100

    Add pasword manager with gui

---
+++ password_manager_tkinter/main.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+# Aleksandr Verevkin
+# Password Manager Project with GUI using tkinter
+import tkinter.messagebox
+from tkinter import *
+import pyperclip
+import random
+START_EMAIL = "placeholder@gmail.com"
+ENTRY_FILE = "data.txt"  # file to save passwords
+
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    # Generate random password and copy it into clipboard
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = [random.choice(letters) for _ in range(nr_letters)]
+    password_list += [random.choice(symbols) for _ in range(nr_symbols)]
+    password_list += [random.choice(numbers) for _ in range(nr_numbers)]
+
+    random.shuffle(password_list)
+    password = ''.join(password_list)
+    inp_password.delete(0, 'end')
+    inp_password.insert(0, password)
+    pyperclip.copy(password)
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_password():
+    # Save password into .txt
+    website = inp_website.get()
+    email = inp_email.get()
+    password = inp_password.get()
+    if not website or not email or not password:
+        tkinter.messagebox.showwarning(title="Empty field", message="Please don't leave any fields empty")
+        return
+    if tkinter.messagebox.askokcancel(title="Assertion", message=f"Entered details:\nWebsite: {website}\n"
+                                                                 f"Email/Username: {email}\nPassword: {password}\n"
+                                                                 f"Save?"):
+        with open(ENTRY_FILE, "a") as f:
+            f.write(f"{inp_website.get()} | {inp_email.get()} | {inp_password.get()}\n")
+            tkinter.messagebox.showinfo(title="Success", message=f"Entry was saved into {ENTRY_FILE}")
+        inp_password.delete(0, 'end')
+        inp_website.delete(0, 'end')
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+if __name__ == "__main__":
+    # Main window
+    window = Tk()
+    window.config(padx=40, pady=40)
+    window.title("Password Manager")
+    # Logo
+    canvas = Canvas(width=200, height=200, highlightthickness=0)
+    lock_pic = PhotoImage(file="logo.png")
+    canvas.create_image(100, 100, image=lock_pic)
+    canvas.grid(row=0, column=1, sticky="w")
+    # Website label
+    l_website = Label(text="Website:", highlightthickness=0)
+    l_website.grid(column=0, row=1)
+    # Email/Username label
+    l_email = Label(text="Email/Username:", highlightthickness=0)
+    l_email.grid(column=0, row=2)
+    # Password label
+    l_password = Label(text="Password:", highlightthickness=0)
+    l_password.grid(column=0, row=3)
+    # Website entry
+    inp_website = Entry(width=36, justify='center')
+    inp_website.grid(column=1, row=1, columnspan=2, sticky='w')
+    inp_website.focus()
+    # Email/Username entry
+    inp_email = Entry(width=36, justify='center')
+    inp_email.grid(column=1, row=2, columnspan=2, sticky='w')
+    inp_email.insert(0, START_EMAIL)
+    # Password entry
+    inp_password = Entry(width=21)
+    inp_password.grid(column=1, row=3, columnspan=2, sticky='w')
+    # Generate Password button
+    but_generate = Button(text="Generate Password", width=14, command=generate_password)
+    but_generate.grid(column=1, row=3, columnspan=2, sticky='e')
+    # Add button
+    but_add = Button(text="Add", width=33, command=save_password)
+    but_add.grid(column=1, row=4, columnspan=2, sticky='w')
+
+    window.mainloop()
