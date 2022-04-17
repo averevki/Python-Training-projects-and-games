44c45fbdb76179b35e2ccb482ed8fb74b0e646b4
Verevkin-A <sandyverevkin@gmail.com>
2022-04-17 22:15:57 +0200

    feat: add kick and ban commands

--- TCP_chat_room/client.py
+++ TCP_chat_room/client.py
@@ -1,40 +1,59 @@
 #!/usr/bin/env python3
 
-from threading import Thread
-import socket
 from sys import argv
+import socket
+from threading import Thread, Lock
 
-if len(argv) != 2:
+if len(argv) > 2:
     print("Usage: ./client.py [PORT]")
     exit(1)
 
 HOST = "127.0.0.1"  # localhost
-try:
-    PORT = int(argv[1])     # free port
-    if PORT < 0 or str(PORT) != argv[1]:
-        raise ValueError
-except ValueError:
-    print("PORT must be a whole, positive number")
-    exit(1)
+PORT = 54321    # standard port
+if len(argv) == 2:
+    try:
+        PORT = int(argv[1])     # free port
+        if PORT < 0 or str(PORT) != argv[1]:
+            raise ValueError
+    except ValueError:
+        print("PORT must be a whole, positive number")
+        exit(1)
 
 client = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 client.connect((HOST, PORT))    # server connection
 
 nickname: str = "unknown"
+STOP_THREAD = False
 
 
 def receive():
     """Listening to server messages"""
+    global STOP_THREAD
     while True:
+        if STOP_THREAD is True:
+            break
         try:
-            msg = client.recv(1024).decode("ascii")
+            msg = client.recv(1024).decode("utf-8")
             if msg == "Your nickname: ":
+                write_lock.acquire()
                 global nickname
                 nickname = input(msg)
-                client.send(nickname.encode("ascii"))
+                client.send(nickname.encode("utf-8"))
+                nxt_msg = client.recv(1024).decode("utf-8")
+                if nxt_msg == "Password: ":
+                    client.send(input(nxt_msg).encode("utf-8"))
+                    if client.recv(1024).decode("utf-8") == "Wrong password":
+                        print("Wrong password")
+                        STOP_THREAD = True
+                else:
+                    print(nxt_msg)
+                    if nxt_msg == "You are BANNED":
+                        client.close()
+                        STOP_THREAD = True
+                write_lock.release()
             else:
                 print(msg)
-        except:
+        except Exception:
             print("An ERROR occurred")
             client.close()
             break
@@ -42,10 +61,18 @@ def receive():
 
 def write():
     """Write new message into the chat"""
+    write_lock.acquire()    # wait for authentication
     while True:
+        if STOP_THREAD:
+            break
         msg = input()
-        client.send(f"{nickname}: {msg}".encode("ascii"))
+        if msg.startswith("/"):
+            client.send(msg.encode("utf-8"))
+        else:
+            client.send(f"{nickname}: {msg}".encode("utf-8"))
 
+
+write_lock = Lock()     # authorization lock
 
 recv_thread = Thread(target=receive)
 recv_thread.start()
--- TCP_chat_room/server.py
+++ TCP_chat_room/server.py
@@ -1,45 +1,66 @@
 #!/usr/bin/env python3
 
-import threading
-import socket
 from sys import argv
+import socket
+import threading
 
-if len(argv) != 2:
+if len(argv) > 2:
     print("Usage: ./server.py [PORT]")
     exit(1)
 
 HOST = "127.0.0.1"  # localhost
-try:
-    PORT = int(argv[1])     # free port
-    if PORT < 0 or str(PORT) != argv[1]:
-        raise ValueError
-except ValueError:
-    print("PORT must be a whole, positive number")
-    exit(1)
+PORT = 54321    # standard port
+if len(argv) == 2:
+    try:
+        PORT = int(argv[1])     # free port
+        if PORT < 0 or str(PORT) != argv[1]:
+            raise ValueError
+    except ValueError:
+        print("PORT must be a whole, positive number")
+        exit(1)
 
 
 server = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 server.listen()
 
-clients: list[socket] = []
+clients: list[socket.socket] = []
 nicknames: list[str] = []
 
 
 def broadcast(msg: str):
-    """Broadcasting message to all connected clients"""
-    msg = msg.encode("ascii")   # encoding message
+    """Send message to all connected clients"""
+    msg = msg.encode("utf-8")   # encoding message
     for client in clients:
         client.send(msg)
 
 
-def handle(client):
+def handle(client: socket.socket):
+    """Receiving users messages"""
     while True:
         try:
-            msg = client.recv(1024).decode("ascii")
+            msg: str = client.recv(1024).decode("utf-8")
             if not msg:
-                raise Exception
-            broadcast(msg)
+                raise ConnectionError
+            if msg.startswith("/"):
+                if nicknames[clients.index(client)] == "admin":
+                    if msg.startswith("/kick "):
+                            nickname = msg[6:]
+                            if nickname in nicknames:
+                                broadcast(f"{nickname} was KICKED by admin")
+                                kick_user(nickname)
+                    elif msg.startswith("/ban "):
+                        if nicknames[clients.index(client)] == "admin":
+                            nickname = msg[5:]
+                            if nickname in nicknames:
+                                broadcast(f"{nickname} was BANNED by admin")
+                                kick_user(nickname)
+                            with open("bans.txt", "a") as f:
+                                f.write(nickname + "\n")
+                else:
+                    client.send("Insufficient permissions".encode("utf-8"))
+            else:
+                broadcast(msg)
         except Exception:
             i = clients.index(client)
             clients.remove(client)
@@ -48,27 +69,54 @@ def handle(client):
             print(f"{nickname}: connection lost")
             broadcast(f"{nickname} has left the chat")
             nicknames.pop(i)
-            print(nicknames)
-            print(clients)
             break
 
 
 def receive():
+    """
+    Listening for new connections and
+    assigning for them threads"""
     while True:
         client, address = server.accept()
         print(f"New connection: {address}")
 
-        client.send("Your nickname: ".encode("ascii"))
-        nickname = client.recv(1024).decode("ascii")
+        client.send("Your nickname: ".encode("utf-8"))
+        nickname = client.recv(1024).decode("utf-8")
+
+        with open("bans.txt", "r") as f:
+            banned = f.read().splitlines()
+        if nickname in banned:
+            client.send("You are BANNED".encode("utf-8"))
+            client.close()
+            continue
+
+        if nickname == "admin":
+            client.send("Password: ".encode("utf-8"))
+            password = client.recv(1024).decode("utf-8")
+
+            if password != "admin":
+                client.send("Wrong password".encode("utf-8"))
+                client.close()
+                continue
+
         nicknames.append(nickname)
         clients.append(client)
         print(f"Client{address} nickname: {nickname}")
 
-        client.send(f"Connected to the chatroom".encode("ascii"))
+        client.send(f"Connected to the chatroom".encode("utf-8"))
         broadcast(f"{nickname} joined the chat room")
 
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
+
+
+def kick_user(nickname: str):
+    """Kick user from server by his nickname"""
+    i = nicknames.index(nickname)
+    client_to_kick = clients.pop(i)
+    client_to_kick.send("Server connection lost".encode("utf-8"))
+    client_to_kick.close()
+    nicknames.pop(i)
 
 
 print("Listening for connections...")
